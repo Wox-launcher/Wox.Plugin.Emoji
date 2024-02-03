cb9540ea70a8e881411f7fb786cc95d0e5165285
qianlifeng <qianlf2010@gmail.com>
2024-02-03 16:01:07 +0800

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Wox.Plugin.Emoji
+
+A simple emoji plugin for Wox.
+
+![snapshot](snapshot/snapshot.png)
+
+## Install
+
+`wpm install Emoji`
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,41 @@
-import type {Plugin, PluginInitContext, Query, Result} from "@wox-launcher/wox-plugin"
+import type { Plugin, PluginInitContext, Query, Result } from "@wox-launcher/wox-plugin"
 import emoji from "emojilib"
-import clipboard from 'clipboardy';
+import clipboard from "clipboardy"
 
 export const plugin: Plugin = {
-    init: async (context: PluginInitContext) => {
-    },
+  init: async (context: PluginInitContext) => {},
 
-    query: async (query: Query): Promise<Result[]> => {
-        let search = query.Search
-        if (query.Search === "") {
-            search = "smile"
-        }
-
-        return Object.keys(emoji).filter((key) => {
-
-            // @ts-ignore
-            const emojiData = emoji[key] as string[]
-            return emojiData.some((keyword: string) => {
-                return keyword.includes(search)
-            })
-        }).map((key) => {
-            return {
-                // @ts-ignore
-                Title: (emoji[key][0] as string).replace(/_/g, " "),
-                Icon: {
-                    ImageType: "emoji",
-                    ImageData: key
-                },
-                Actions: [
-                    {
-                        Name: "Copy",
-                        Action: async () => {
-                            await clipboard.write(key)
-                        }
-                    }
-                ]
-            } as Result
-        })
+  query: async (query: Query): Promise<Result[]> => {
+    let search = query.Search
+    if (query.Search === "") {
+      search = "smile"
     }
+
+    return Object.keys(emoji)
+      .filter(key => {
+        // @ts-ignore
+        const emojiData = emoji[key] as string[]
+        return emojiData.some((keyword: string) => {
+          return keyword.includes(search)
+        })
+      })
+      .map(key => {
+        return {
+          // @ts-ignore
+          Title: (emoji[key][0] as string).replace(/_/g, " "),
+          Icon: {
+            ImageType: "emoji",
+            ImageData: key
+          },
+          Actions: [
+            {
+              Name: "Copy",
+              Action: async () => {
+                await clipboard.write(key)
+              }
+            }
+          ]
+        } as Result
+      })
+  }
 }
